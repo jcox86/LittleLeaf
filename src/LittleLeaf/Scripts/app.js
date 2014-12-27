06c6b2f2e1b60530c723c98cd4754153ca73eb22
Jake Cox <jake.cox@lotpath.com>
2014-12-27 10:06:47 -0800

    lumx install

---
+++ src/LittleLeaf/Scripts/app.js
@@ -0,0 +1,21 @@
+﻿var app = angular.module('app', [
+    'ngRoute',
+    'lumx'
+]);
+
+app.filter('moment', function () {
+    return function (dateString, format) {
+        return moment(dateString).format(format);
+    };
+});
+
+app.run(['$route', '$q', '$rootScope',
+    function ($route, $q, $rootScope) {
+    }
+]);
+
+app.filter('capitalize', function () {
+    return function (input, scope) {
+        return input.substring(0, 1).toUpperCase() + input.substring(1);
+    };
+});
